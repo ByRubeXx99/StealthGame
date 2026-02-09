9b91e10484a0d9ff4bf08616d46422d794245b6b
NANEDCA <dhernandezr@edu.tecnocampus.cat>
2026-02-09 16:50:05 +0100

    enemies+movment

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float  Speed = 3;
+    public LayerMask wall;
+    public Vector2 Idirection=Vector2.right;
+    // Update is called once per frame 
+    private void Update()
+    {
+        Patrol();
+      if(Collision()){ transform.Rotate(0, 0, 180); Idirection=Idirection*-1; }
+    } 
+    private void Patrol()
+    {
+        transform.Translate(Idirection*Speed*Time.deltaTime,Space.World);
+    }
+    private bool Collision()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,Idirection,1f,wall); 
+        if (hit.collider!=null)
+        {
+            Debug.Log("paret trobada");
+            return true;
+        } 
+        return false;
+    }
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, Idirection, Color.red);
+    }
+}
