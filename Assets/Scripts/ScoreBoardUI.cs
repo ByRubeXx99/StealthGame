49e59aa7dcc33237b1d8f0b247ca45a5ee1a4c1e
RubenMaBe <204864661+ByRubeXx99@users.noreply.github.com>
2026-02-11 00:03:44 +0100

    Mejoras en el ending scene
    He corregido la UI de la ending scene, añadiendo también el tiempo total

--- Assets/Scripts/ScoreBoardUI.cs
+++ Assets/Scripts/ScoreBoardUI.cs
@@ -4,35 +4,46 @@ using System.Collections;
 
 public class ScoreBoard : MonoBehaviour
 {
-    [Header("Configuración UI")]
-    public TextMeshProUGUI scoreText;
-    public GameObject expedientePanel; 
+    [Header("UI References")]
+    public TextMeshProUGUI ScoreText;    
+    public TextMeshProUGUI TimeText;   
+    public GameObject FilePanel;    
 
-    [Header("Ajustes")]
-    public int puntuacionFinal = 1500; 
-    public float duracionAnimacion = 2.0f; 
+    [Header("Settings")]
+    public float AnimationDuration = 2.0f; 
 
-    void Start()
+    private void Start()
     {
-        scoreText.text = "0000";
-        StartCoroutine(AnimarPuntuacion());
+        int finalScore = PlayerPrefs.GetInt("Score", 0);
+        float finalTime = PlayerPrefs.GetFloat("FinalTime", 0f);
+       
+        ScoreText.text = "0000";
+        TimeText.text = FormatTime(finalTime); 
+
+        StartCoroutine(AnimateScore(finalScore));
     }
-    IEnumerator AnimarPuntuacion()
+    private string FormatTime(float timeInSeconds)
     {
-        yield return new WaitForSeconds(1.5f);
+        float minutes = Mathf.FloorToInt(timeInSeconds / 60);
+        float seconds = Mathf.FloorToInt(timeInSeconds % 60);
 
-        float tiempoPasado = 0f;
-        int valorInicial = 0;
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    private IEnumerator AnimateScore(int targetScore)
+    {
+        yield return new WaitForSeconds(2f); 
+        float timeElapsed = 0f;
+        int startValue = 0;
 
-        while (tiempoPasado < duracionAnimacion)
+        while (timeElapsed < AnimationDuration)
         {
-            tiempoPasado += Time.deltaTime;
-            float porcentaje = tiempoPasado / duracionAnimacion;
-            int valorActual = (int)Mathf.Lerp(valorInicial, puntuacionFinal, porcentaje);
-            
-            scoreText.text = valorActual.ToString("D4"); 
-            yield return null; 
+            timeElapsed += Time.deltaTime;
+            float percentage = timeElapsed / AnimationDuration;
+            int currentValue = (int)Mathf.Lerp(startValue, targetScore, percentage);
+
+            ScoreText.text = currentValue.ToString("D4"); 
+            yield return null;
         }
-        scoreText.text = puntuacionFinal.ToString("D4");
+        ScoreText.text = targetScore.ToString("D4");
     }
 }
