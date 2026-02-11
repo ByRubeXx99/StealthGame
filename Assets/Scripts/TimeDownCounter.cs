3554e022d08461731d920daf14ee368498b9ddf7
JaviBarceloo <javibarcelo02@gmail.com>
2026-02-11 13:29:27 +0100

    Timer funciona
    Timer funciona

---
+++ Assets/Scripts/TimeDownCounter.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public float startTime = 30f;
+
+    private float currentTime;
+    private bool timeUp = false;
+
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    void Update()
+    {
+        if (timeUp) return;
+
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+
+            if (currentTime < 0)
+                currentTime = 0;
+
+            timerText.text = "Time: " + (int)currentTime;
+        }
+
+        if (currentTime <= 0 && !timeUp)
+        {
+            timeUp = true;
+            OnTimeUp();
+        }
+    }
+
+    void OnTimeUp()
+    {
+        SceneManager.LoadScene("Ending");
+    }
+}
